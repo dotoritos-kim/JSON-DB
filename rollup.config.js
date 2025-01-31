7b7ea11becd4b343c0494811aaa9854f4a4f5b3a
Dotoritos <wwponv158@gmail.com>
2025-01-31 19:54:12 +0900

    fix: add dynamicImport

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import obfuscator from "rollup-plugin-obfuscator";
+import dynamicImport from "rollup-plugin-dynamic-import-variables";
 
 export default [
 	{
@@ -12,6 +13,7 @@ export default [
 			format: "esm",
 			sourcemap: true,
 			preserveModules: true, // ✅ 모듈 구조 유지
+			preserveModulesRoot: "src",
 			entryFileNames: "[name].js", // 파일명 유지
 		},
 		plugins: [
@@ -22,6 +24,7 @@ export default [
 				compress: { drop_console: true, drop_debugger: true },
 				output: { comments: false },
 			}),
+			dynamicImport(),
 			obfuscator({
 				compact: true,
 				controlFlowFlattening: false,
@@ -43,12 +46,14 @@ export default [
 			format: "cjs",
 			sourcemap: true,
 			preserveModules: true, // 모듈 유지
+			preserveModulesRoot: "src",
 			entryFileNames: "[name].cjs.js",
 		},
 		plugins: [
 			resolve(),
 			commonjs(),
 			typescript(),
+			dynamicImport(),
 			terser({
 				compress: { drop_console: true, drop_debugger: true },
 				output: { comments: false },
